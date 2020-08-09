c5d5857a17c65f4afb5b4d832e07156819f2539e
pacna <paulc1231@gmail.com>
2020-08-09 02:58:09 -0400

    Add tests and update directory

---
+++ tests/ShuntingYard.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ShuntingYard.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("FOO", "FOO");
+        }
+    }
+}
